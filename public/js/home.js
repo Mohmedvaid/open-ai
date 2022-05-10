eb762d13cf2ac3c355a752ec149e295e6aa9c73d
mohmedvaid <hussainvaid7@gmail.com>
2022-05-09 22:34:58 -0500

    added api call

--- public/js/home.js
+++ public/js/home.js
@@ -1,7 +1,37 @@
-$(document).ready(function() {
-  $('#formMain').submit(function(e) {
+$(document).ready(function () {
+  // get prompt
+  $("#formMain").submit(function (e) {
     e.preventDefault();
-    var form = $(this);
-    console.log(form.serialize());
-  })
-})
+    const prompt = getUserPrompt();
+    const data = generateData(prompt);
+    sendRequest(data)
+      .then((data) => console.log(data))
+      .catch((err) => console.error(err));
+  });
+});
+
+function getUserPrompt() {
+  return $("textarea[name='userPrompt']").val().trim();
+}
+
+function sendRequest(data) {
+  return fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer APIKEY`,
+    },
+    body: JSON.stringify(data),
+  });
+}
+
+function generateData(prompt, config) {
+  return (data = {
+    prompt,
+    temperature: 0.5,
+    max_tokens: 64,
+    top_p: 1.0,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0,
+  });
+}
